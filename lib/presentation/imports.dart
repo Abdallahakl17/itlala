786e3c0d18c367bc29c1514882770b235a83143f
Abdallahakl17 <dalla.dd123@yahoo.com>
2024-07-22 00:55:00 +0300

    create navigation

--- lib/presentation/imports.dart
+++ lib/presentation/imports.dart
@@ -13,4 +13,13 @@ export 'package:itlala/core/theme/app_theme.dart';
 export 'package:itlala/presentation/widget/auth/textform_feild.dart';
 export 'package:itlala/presentation/widget/custom_buttons.dart';
 export 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
- 
+export 'package:itlala/core/router/app_routes_name.dart';
+export 'package:itlala/presentation/imports.dart';
+export 'package:itlala/presentation/screens/auth/Password_Changed.dart';
+export 'package:itlala/presentation/screens/auth/create_new_password.dart';
+export 'package:itlala/presentation/screens/auth/forget_password.dart';
+export 'package:itlala/presentation/screens/auth/otp_verification.dart';
+export 'package:itlala/presentation/screens/auth/singup.dart';
+export 'package:itlala/presentation/screens/home/home_page.dart';
+export 'package:itlala/presentation/screens/onboarding.dart';
+export 'package:itlala/presentation/screens/splash.dart';
--- lib/presentation/screens/splash.dart
+++ lib/presentation/screens/splash.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:itlala/core/utils/constant/app_colors.dart';
 import 'package:itlala/core/utils/constant/app_images.dart';
-
- import 'package:itlala/presentation/screens/onboarding.dart';
+import 'package:itlala/presentation/screens/onboarding.dart';
 
 class LogoScreen extends StatefulWidget {
   const LogoScreen({super.key});
@@ -13,36 +12,71 @@ class LogoScreen extends StatefulWidget {
 }
 
 class _LogoScreenState extends State<LogoScreen> {
+  double _opacity = 0.0;
+  Alignment _alignment = Alignment.topCenter;
+
   @override
   void initState() {
-    Future.delayed(const Duration(seconds: 4), () {
-      Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (context) =>   OnboardingPage(),
-          ));
-    });
     super.initState();
+    Future.delayed(Duration.zero, () {
+      setState(() {
+        _opacity = 1.0;
+        _alignment = Alignment.center;
+      });
+    });
+
+    // Future.delayed(const Duration(seconds: 4), () {
+    //   Navigator.pushReplacement(
+    //     context,
+    //     MaterialPageRoute(
+    //       builder: (context) => OnboardingPage(),
+    //     ),
+    //   );
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: Container(
-          color: AppColor.whiteColor,
-          child:   Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                  AppImages.logo,
-                  width: double.infinity,
-                ),
-                Text('ITLALA',style: TextStyle(color: AppColor.blackColor,fontSize: 30.sp),)
-            ],
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                AppColor.linearProgress,
+                AppColor.buttonsColor,
+                AppColor.ligthBlue,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
           ),
-         
+          child: AnimatedAlign(
+            duration: const Duration(seconds: 4),
+            alignment: _alignment,
+            curve: Curves.easeInOut,
+            child: AnimatedOpacity(
+              duration: const Duration(seconds: 4),
+              opacity: _opacity,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 40.w,
+                    child: Image.asset(
+                      AppImages.logo,
+                      width: double.infinity,
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Text(
+                    'ITLALA',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
